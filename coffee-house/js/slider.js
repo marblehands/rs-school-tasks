74c273f28ef20b88a768ef5b56c45a03b00ba519
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-09 23:04:10 +0100

    feat: add progress bars animation

--- coffee-house/js/slider.js
+++ coffee-house/js/slider.js
@@ -1,36 +1,88 @@
-let slideIndex = 0;
+let index = 0;
 
 const slidesBox = document.querySelector('.slides-wrapper');
 const progressbarsBox = document.querySelectorAll('.slider-pagination-item');
 const progressbars = document.querySelectorAll('.progress-bar');
-console.log(progressbars)
 
-const progressbarScaling = [
-  { width: "100%" },
-];
-
-const progressbarScalingBack = [
-  { width: "100%" },
-  { width: "0%" },
-];
-
-const progressbarTiming = {
-  duration: 3000,
-  iteration: 1,
-};
-
+// progressbars[slideIndex].addEventListener('transitionend', () => {
+//   slideIndex === 0 ? progressbars[2].style.width = '0%' : progressbars[slideIndex - 1].style.width = '0%';
+//   moveSlides()
+// })
 
 function moveSlides() {
-  if (slideIndex < 3) {
-    slidesBox.style.marginLeft = (`-${slideIndex}00%`);
-    progressbars[slideIndex].animate(progressbarScaling, progressbarTiming);
-  } else {
-    slideIndex = 0;
-    slidesBox.style.marginLeft = (`-${slideIndex}00%`);
-    progressbars[slideIndex].animate(progressbarScaling, progressbarTiming);
-  }
-  slideIndex++;
+  slidesBox.style.marginLeft = (`-${index}00%`);
+  progressbars[index].style.transform = 'scaleX(0)';
+  index++;
+  index >= 3 ? (index = 0) : index;
+  fullBar();
 }
 
-moveSlides()
-setInterval(moveSlides, 3000)
+function fullBar() {
+  let bar = progressbars[index];
+  bar.style.transform = 'scaleX(2)';
+  let width = 1;
+  const increase = setInterval(() => {
+    bar.style.width = `${width}%`;
+    width++;
+    console.log(width)
+  if (width > 100) {
+    clearInterval(increase);
+    moveSlides();
+  }
+  }, 30);
+}
+
+fullBar();
+
+
+
+// moveSlides();
+
+// let start = Date.now();
+
+// let timer = setInterval(function () {
+//   let timePassed = Date.now() - start;
+//   if (timePassed >= 3000) {
+//     clearInterval(timer);
+//     return;
+//   }
+//   draw(timePassed);
+// }, 20);
+
+// function draw(timePassed) {
+//   progressbars[index].style.width = timePassed / 25 + 'px';
+// }
+
+
+
+
+// function animateBar() {
+//   const element = progressbars[slideIndex];
+//   element.style.width = '0%';
+//   let currentWidth = 0;
+//   let widthStep = 1;
+//   currentWidth += 1;
+//   element.style.width = `${currentWidth}%`;
+// }
+
+// setInterval(animateBar, 20)
+
+// progressbars.forEach((item)=>{
+//   item.addEventListener('animationend', ()=>{
+//     moveSlides()
+//   })
+// })
+
+// moveSlides()
+// hideWidth()
+// setInterval(moveSlides, 3000)
+// setInterval(hideWidth, 3000)
+
+
+// function hideWidth() {
+//   progressbars.forEach((item)=>{
+//     if (item.width === '100%') {
+//       item.style.animation = 'emptyBar 1s forwards';
+//     }
+//   })
+// }
