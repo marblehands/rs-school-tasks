f97dfb6c167e104299e178d361fcd93dd3928e03
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-11 04:30:36 +0100

    fix: burger menu layout

--- coffee-house/js/burger.js
+++ coffee-house/js/burger.js
@@ -1,16 +1,47 @@
+const body = document.getElementsByTagName('body');
 const burger = document.querySelector('.btn-burger');
 const burgerLines = document.querySelectorAll('.btn-burger-line');
 const navigation = document.querySelector('.main-nav');
-const header = document.querySelector('.header');
+const linkMenu = document.querySelector('.link-active');
 
-burger.addEventListener('click', () => {
+let burgerState = 0;
+
+console.log(body)
+
+burger.addEventListener('click', (event) => {
+  event.stopPropagation();
+    openBurger();
+})
+
+burgerLines.forEach((line) => {
+  line.addEventListener('click', (event) => {
+    event.stopPropagation();
+    openBurger();
+  })
+})
+
+navigation.addEventListener('click', (event) => {
+  event.stopPropagation();
+  closeBurger();
+});
+
+body[0].addEventListener('click', (event) => {
+  event.stopPropagation();
+  if (burgerState && event.target !== burger) {
+    closeBurger();
+  }
+})
+
+function openBurger() {
+  burgerState = 1;
   burger.classList.toggle('burger-open');
   navigation.classList.toggle('nav-open');
   document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-})
+}
 
-navigation.addEventListener('click', () => {
+function closeBurger() {
+  burgerState = 0;
   burger.classList.remove('burger-open');
   navigation.classList.remove('nav-open');
   document.getElementsByTagName('body')[0].style.overflow = 'visible';
-});
+}
