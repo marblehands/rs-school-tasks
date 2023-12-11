3c4c8bc9145c56f5199e7f01e018e91a5e820041
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-11 05:35:50 +0100

    feat: burger menu smoothly hides when clicking on any link

--- coffee-house/js/burger.js
+++ coffee-house/js/burger.js
@@ -2,11 +2,15 @@ const body = document.getElementsByTagName('body');
 const burger = document.querySelector('.btn-burger');
 const burgerLines = document.querySelectorAll('.btn-burger-line');
 const navigation = document.querySelector('.main-nav');
-const linkMenu = document.querySelector('.link-active');
+const linkHome = document.querySelector('#menu');
+// const linkFavorite = document.querySelector('#favorite');
+// const linkAbout = document.querySelector('#about');
+// const linkmobile = document.querySelector('#mobile');
+const linksMenu = document.querySelectorAll('[data-link-menu]');
+const linkLogoMenu = document.querySelector('[data-link-logo="menu"]');
+const linkLogoHome = document.querySelector('[data-link-logo="home"]');
 
 let burgerState = 0;
-
-console.log(body)
 
 burger.addEventListener('click', (event) => {
   event.stopPropagation();
@@ -32,6 +36,46 @@ body[0].addEventListener('click', (event) => {
   }
 })
 
+if(linkHome) {
+  linkHome.addEventListener('click', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+      openPage('menu.html')
+    }, 520);
+  })
+}
+
+if(linksMenu) {
+  linksMenu.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const linkMenuValue = link.dataset.linkMenu;
+      let url = `index.html#${linkMenuValue}`
+      setTimeout(() => {
+        openPage(url)
+      }, 520);
+    })
+  })
+}
+
+if(linkLogoMenu) {
+  linkLogoMenu.addEventListener('click', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+      openPage('index.html')
+    }, 520);
+  })
+}
+
+if(linkLogoHome) {
+  linkLogoHome.addEventListener('click', (event) => {
+    event.preventDefault();
+    setTimeout(() => {
+      openPage('index.html')
+    }, 520);
+  })
+}
+
 function openBurger() {
   burgerState = 1;
   burger.classList.toggle('burger-open');
@@ -44,4 +88,8 @@ function closeBurger() {
   burger.classList.remove('burger-open');
   navigation.classList.remove('nav-open');
   document.getElementsByTagName('body')[0].style.overflow = 'visible';
+}
+
+function openPage(link) {
+  window.location.href = `${link}`;
 }
