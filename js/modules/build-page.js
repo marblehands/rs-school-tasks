4aad69be79ef3714a41ab5a85d77b59fd40d3af6
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-29 00:19:42 +0100

    feat: add logo

--- js/modules/build-page.js
+++ js/modules/build-page.js
@@ -1,6 +1,6 @@
 export function createNode (parentNode, tagName, styles = '', content = '') {
   let node = document.createElement(tagName);
-  if (classes) {
+  if (styles) {
     node.className = `${styles}`;
   }
   if (content) {
@@ -10,4 +10,14 @@ export function createNode (parentNode, tagName, styles = '', content = '') {
     parentNode.appendChild(node);
   }
   return node;
+}
+
+
+export function generateHeader() {
+  const body = document.body;
+  let header = createNode(body, 'header', 'header');
+  let logo = createNode(header, 'img', 'img-logo');
+  logo.src = 'assets/svg/logo-hangman.svg';
+  logo.alt = 'Hangman Game Logotype';
+  header.appendChild(logo);
 }
