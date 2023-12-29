cfd54aad927f2572b94f6045ea95db411ec7d1fd
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-29 14:50:02 +0100

    feat: draw basic layout

--- js/modules/build-page.js
+++ js/modules/build-page.js
@@ -1,3 +1,18 @@
+const body = document.body;
+let question;
+let quiz;
+let word;
+let gameContent;
+
+drawHeader();
+drawGameContent();
+drawQuestion();
+drawGallow();
+drawSecretWord('hello');
+drawKeyboard();
+drawGameFooter();
+drawFooter();
+
 export function createNode (parentNode, tagName, styles = '', content = '') {
   let node = document.createElement(tagName);
   if (styles) {
@@ -7,17 +22,93 @@ export function createNode (parentNode, tagName, styles = '', content = '') {
     node.textContent = `${content}`;
   }
   if (parentNode) {
-    parentNode.appendChild(node);
+    if (parentNode === body) {
+      parentNode.append(node);
+    }
+    parentNode.append(node);
   }
   return node;
 }
 
-
-export function generateHeader() {
-  const body = document.body;
+export function drawHeader() {
   let header = createNode(body, 'header', 'header');
   let logo = createNode(header, 'img', 'img-logo');
   logo.src = 'assets/svg/logo-hangman.svg';
   logo.alt = 'Hangman Game Logotype';
   header.appendChild(logo);
+}
+
+export function drawGameContent() {
+  let main = createNode(body, 'main', 'main');
+  gameContent = createNode(main, 'div', 'global-wrapper');
+  main.appendChild(gameContent);
+}
+
+export function drawQuestion() {
+  question = createNode(false, 'div', 'question-wrapper', 'What is your name?');
+  gameContent.appendChild(question);
+}
+
+export function drawGallow() {
+  let gallow = createNode(false, 'div', 'gallow-wrapper');
+  let hangman = createNode(gallow, 'div', 'hangman-wrapper');
+  let head = createNode(hangman, 'div', 'head');
+  let manbody = createNode(hangman, 'div', 'manbody');
+  let leftHand = createNode(hangman, 'div', 'left-hand');
+  let rightHand = createNode(hangman, 'div', 'right-hand');
+  let leftFoot = createNode(hangman, 'div', 'left-foot');
+  let rightFoot = createNode(hangman, 'div', 'right-foot');
+  gallow.appendChild(hangman);
+  gameContent.appendChild(gallow);
+}
+
+export function drawSecretWord(secretWord) {
+  quiz = createNode(false, 'div', 'quiz-wrapper');
+  word = createNode(quiz, 'div', 'word-wrapper');
+  const lettersArray = secretWord.split('');
+
+  lettersArray.forEach((char) => {
+    let letter = createNode(word, 'div', 'letter', char);
+    word.appendChild(letter);
+  });
+
+  gameContent.appendChild(quiz);
+}
+
+export function drawKeyboard() {
+  const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let keyboard = createNode(quiz, 'div', 'keyboard-wrapper');
+
+  let charsArray = ALPHABET.split('');
+
+  charsArray.forEach((char) => {
+    let btn = createNode(keyboard, 'div', 'key-char', char);
+  });
+
+  quiz.appendChild(keyboard);
+}
+
+export function drawGameFooter() {
+  let info = createNode(gameContent, 'div', 'info-wrapper');
+  let guesses = createNode(info, 'div', 'guesses-wrapper');
+  let title = createNode(guesses, 'span', 'guesses-title', 'Incorrect Guesses');
+  let guessesCount = createNode(guesses, 'span', 'guesses-count', '0');
+  let guessesAll = createNode(guesses, 'span', 'gueses-all', '/ 6');
+
+  let lifes = createNode(info, 'div', 'lifes-wrapper');
+  for (let i = 0; i < 7; i++) {
+    let life = createNode(lifes, 'div', 'heart');
+    lifes.appendChild(life);
+  }
+
+  gameContent.appendChild(info);
+}
+
+export function drawFooter() {
+  let footer = createNode(body, 'footer', 'footer');
+  let info = createNode(footer, 'span', '', '&copy; 2023 Anna Chebysheva');
+  let link = createNode(footer, 'a', 'github-link', 'GitHub');
+  link.href = 'https://github.com/marblehands';
+  link.target = '_blank';
+  body.append(footer);
 }
---
+++ js/modules/quiz.js
@@ -0,0 +1,28 @@
+const quiz = [
+  {
+    question: "What data structure processes elements adhering to the FIFO principle?",
+    answer: "Queue"
+  },
+  {
+    question: "The space between two adjacent grid lines (row or column) is called grid ...",
+    answer: "Track"
+  },
+  {
+    question: "What is the term for a JavaScript function that is passed as an argument to another function?",
+    answer: "Callback"
+  },
+  {
+    question: "What term is used to describe a technique in CSS where a background image scrolls slower than the content in the foreground, creating a parallax effect?",
+    answer: "Parallax"
+  },
+  {
+    question: "What is the term for a combination of HTML, CSS, and JavaScript that is packaged for easy distribution and reuse?",
+    answer: "Widget"
+  },
+  {
+    question: "What HTML5 element is used to draw graphics, animations, and other visual elements on a webpage?",
+    answer: "Canvas"
+  }
+];
+
+module.exports = quiz;
