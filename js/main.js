19b8e618fd4fe25a146fccc197ab0218626bb23f
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-12-29 14:50:02 +0100

    feat: draw basic layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import { createNode } from "./modules/build-page.js";
-import { generateHeader } from "./modules/build-page.js";
-
-const body = document.body;
-generateHeader();
-createNode(body, 'footer', 'footer', '2023 Anna Chebysheva Github');
+import { drawHeader } from "./modules/build-page.js";
+import { drawGameContent } from "./modules/build-page.js";
+import { drawQuestion } from "./modules/build-page.js";
+import { drawGallow } from "./modules/build-page.js";
+import { drawSecretWord } from "./modules/build-page.js";
+import { drawKeyboard } from "./modules/build-page.js";
+import { drawGameFooter } from "./modules/build-page.js";
+import { drawFooter } from "./modules/build-page.js";
