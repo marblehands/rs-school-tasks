135663d5745cfeba267d4851573d24438b084fa2
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-25 01:58:35 +0100

    feat: add function that generates image templates

--- nonograms/js/build-page.js
+++ nonograms/js/build-page.js
@@ -1,5 +1,7 @@
 import templates from './templates.js';
 
+let gridItems = [];
+
 export function createBasicNode(parent, tag, attributes, classes) {
   const node = document.createElement(tag);
   if (parent) parent.append(node);
@@ -12,10 +14,18 @@ export function drawBoard(size) {
   const gridWrapper = createBasicNode(0, 'div', 0, 'board-wrapper');
   for (let i = 0; i < size ** 2; i++) {
     const gridItem = createBasicNode(gridWrapper, 'div', 0, 'board-item');
+    gridItem.addEventListener('click', clickHandler);
+    gridItems.push(gridItem);
   }
   document.body.append(gridWrapper);
+
   gridWrapper.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   gridWrapper.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  const button = createBasicNode(0, 'button', 0, 'btn-get-matrix');
+
+  button.addEventListener('click', getMatrix);
+  document.body.append(button);
+  button.style.display = 'none';
 }
 
 export function createEmptyMatrix(size) {
@@ -27,4 +37,29 @@ export function createEmptyMatrix(size) {
   return matrix;
 }
 
+function clickHandler(event) {
+  const item = event.currentTarget;
+  item.style.backgroundColor = 'black';
+  item.coloured = 'true';
+  console.log(item.property);
+}
+
 drawBoard(5);
+
+function getMatrix() {
+  const gridItems = document.querySelectorAll('.board-item');
+  let arr = [];
+  Array.from(gridItems).map((item) => {
+    if (item.coloured) {
+      arr.push(1);
+    } else {
+      arr.push(0);
+    }
+    return item;
+  });
+  let matrix = [];
+  for (let i = 0; i < 21; i += 5) {
+    matrix.push(arr.slice(i, i + 5));
+  }
+  return console.log(JSON.stringify({ size: 5, name: 'hash', map: matrix }));
+}
--- nonograms/js/main.js
+++ nonograms/js/main.js
@@ -1,4 +1,3 @@
 import { createBasicNode } from './build-page.js';
 import { drawBoard } from './build-page.js';
 import { createEmptyMatrix } from './build-page.js';
-import { drawImgMatrix } from './build-page.js';
