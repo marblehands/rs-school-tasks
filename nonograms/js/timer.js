559b3452fa321a0ecf2e3e6ef42bc1b8278f5d3f
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-28 01:57:58 +0100

    feat: add working watch

--- nonograms/js/timer.js
+++ nonograms/js/timer.js
@@ -2,11 +2,22 @@ import { createBasicNode } from './build-page.js';
 
 export function drawWatch(main) {
   const wrapper = createBasicNode(main, 'div', 'timer__wrapper');
-  const timer = createBasicNode(wrapper, 'div', 'timer');
-  const minutes = createBasicNode(timer, 'span', 'timer__minutes', '00');
-  minutes.id = 'minutes';
-  const divider = createBasicNode(timer, 'span', ' ', ':');
-  const seconds = createBasicNode(timer, 'span', 'timer__seconds', '00');
-  seconds.id = seconds;
+  const timer = createBasicNode(wrapper, 'span', 'timer', '00:00');
   const pause = createBasicNode(wrapper, 'div', 'timer__icon_pause');
 }
+
+const startTime = Date.now();
+
+export function getTime() {
+  const timer = document.querySelector('.timer');
+  const currentTime = Date.now();
+  const diff = currentTime - startTime;
+  let seconds = Math.floor(diff / 1000);
+  let minutes = Math.floor(seconds / 60);
+  if (minutes) seconds -= minutes * 60;
+  timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}`;
+}
+
+setInterval(getTime, 1000);
