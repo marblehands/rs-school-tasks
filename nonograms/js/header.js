cc22864c07eef33842b059452ae429eac1ad6dfa
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-29 01:04:58 +0100

    feat: add sound mute button

--- nonograms/js/header.js
+++ nonograms/js/header.js
@@ -1,19 +1,34 @@
 import { createBasicNode } from './build-page.js';
 import { changeTheme } from './change-theme.js';
+import { toggleSound } from './audio.js';
 
 export function drawHeader() {
   const headerWrapper = createBasicNode(0, 'header', 'header');
-  // eslint-disable-next-line no-unused-vars
+
   const logo = createBasicNode(headerWrapper, 'div', 'logo', 'NONO GAME');
   logo.addEventListener('click', changeTheme);
-  // eslint-disable-next-line no-unused-vars
-  const settingsWrapper = createBasicNode(headerWrapper, 'nav', 'header__nav');
-  drawButton(settingsWrapper, 'header__btn header__btn_sound');
-  drawButton(settingsWrapper, 'header__btn header__btn_settings');
-  drawButton(settingsWrapper, 'header__btn header__btn_score');
-}
 
-function drawButton(parent, classes = '', content = '') {
-  // eslint-disable-next-line no-unused-vars
-  const btn = createBasicNode(parent, 'button', classes, content);
+  const settingsWrapper = createBasicNode(headerWrapper, 'nav', 'header__nav');
+
+  const soundBtn = createBasicNode(
+    settingsWrapper,
+    'button',
+    'header__btn header__btn_sound'
+  );
+  soundBtn.aliasLabel = 'turn on and turn off sound effects';
+  soundBtn.addEventListener('click', toggleSound);
+
+  const settingsBtn = createBasicNode(
+    settingsWrapper,
+    'button',
+    'header__btn header__btn_settings'
+  );
+  settingsBtn.aliasLabel = 'open game settings modal window';
+
+  const scoreBtn = createBasicNode(
+    settingsWrapper,
+    'button',
+    'header__btn header__btn_score'
+  );
+  scoreBtn.aliasLabel = 'open score table';
 }
