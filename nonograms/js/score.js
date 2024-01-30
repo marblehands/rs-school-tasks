8c3b3fe991a6384f14eb8527e9953e8e9328f5a5
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-01-30 22:43:45 +0100

    feat: generate static layout for score table

--- nonograms/js/score.js
+++ nonograms/js/score.js
@@ -2,13 +2,42 @@ import { createBasicNode } from './build-page.js';
 
 let isScore = false;
 
-function drawScore() {
+export function drawScore() {
+  const WINS = 5;
   const modalWrapper = createBasicNode(0, 'div', 'modal modal__score');
   const modalContent = createBasicNode(modalWrapper, 'div', 'modal__content');
   // eslint-disable-next-line no-unused-vars
   const title = createBasicNode(modalContent, 'h2', 'modal__title', `Score`);
+  drawTable(modalContent, WINS);
 }
 
+function drawTable(parent, rows) {
+  const table = createBasicNode(parent, 'table', 'table');
+  const thead = createBasicNode(table, 'thead');
+  const tr = createBasicNode(thead, 'tr', 'table__header');
+  const titles = ['Puzzle', 'Difficulty', 'Time'];
+  titles.forEach((title) =>
+    createBasicNode(tr, 'th', 'table__headline', title)
+  );
+  const tbody = createBasicNode(table, 'tbody');
+  let count = rows;
+  while (count) {
+    drawRow(tbody, titles.length);
+    count--;
+  }
+}
+
+function drawRow(parent, columns) {
+  const row = createBasicNode(parent, 'tr');
+  const dataAttributes = ['puzzle', 'level', 'time'];
+  for (let i = 0; i < columns; i++) {
+    const cell = createBasicNode(row, 'th', 'table__cell', '--', {
+      'data-name': dataAttributes[i],
+    });
+  }
+}
+
+//there is the same function toggleSettings, maybe better to refactor these two into one
 export function toggleScore(event) {
   const btn = event.currentTarget;
   btn.classList.toggle('checked');
