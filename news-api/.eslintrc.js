1d2bc5bdac5eadb49944f91b99262162cc7ae06d
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-02-24 23:47:59 +0100

    feat: add eslint rules

--- news-api/.eslintrc.js
+++ news-api/.eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
     'airbnb-base',
     'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
@@ -27,9 +27,8 @@ module.exports = {
     sourceType: 'module',
     project: 'tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 2,
     'prettier/prettier': [
       'warn',
       {
@@ -37,5 +36,204 @@ module.exports = {
         useTabs: false,
       },
     ],
+    'max-lines-per-function': [
+      'error',
+      {
+        max: 40,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'no-return-await': 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    curly: 'error',
+    'object-shorthand': 'error',
+    'require-atomic-updates': 'error',
+    'max-statements': ['error', 20],
+    'max-nested-callbacks': ['error', 4],
+    'no-useless-call': 'error',
+    'no-negated-condition': 'error',
+    'no-negated-condition': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-constant-binary-expression': 'error',
+    'import/prefer-default-export': 'off',
+    'import/no-empty-named-blocks': 'error',
+    'import/no-useless-path-segments': [
+      'error',
+      {
+        noUselessIndex: true,
+      },
+    ],
+    'import/no-duplicates': [
+      'error',
+      {
+        'prefer-inline': true,
+        considerQueryString: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'object', 'type', 'index'],
+      },
+    ],
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'off',
+          methods: 'explicit',
+          parameterProperties: 'explicit',
+          properties: 'explicit',
+        },
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
   },
+  '@typescript-eslint/naming-convention': [
+    'error',
+    {
+      selector: 'default',
+      format: ['strictCamelCase'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'variable',
+      format: ['strictCamelCase', 'UPPER_CASE'],
+      modifiers: ['const'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'variable',
+      format: ['strictCamelCase', 'UPPER_CASE'],
+      modifiers: ['const'],
+      types: ['boolean', 'string', 'number'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: ['variable', 'property'],
+      types: ['boolean'],
+      format: ['PascalCase'],
+      prefix: ['is', 'has', 'should', 'can', 'will'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'variable',
+      modifiers: ['destructured'],
+      format: null,
+    },
+    {
+      selector: 'objectLiteralProperty',
+      format: null,
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'parameter',
+      format: ['strictCamelCase'],
+      leadingUnderscore: 'allow',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: ['typeLike', 'enum'],
+      format: ['PascalCase'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+    {
+      selector: 'enumMember',
+      format: ['UPPER_CASE'],
+      leadingUnderscore: 'forbid',
+      trailingUnderscore: 'forbid',
+    },
+  ],
+  '@typescript-eslint/no-explicit-any': 'error',
+  '@typescript-eslint/prefer-includes': 'error',
+  '@typescript-eslint/no-base-to-string': 'error',
+  '@typescript-eslint/no-dynamic-delete': 'error',
+  '@typescript-eslint/no-invalid-void-type': 'error',
+  '@typescript-eslint/prefer-function-type': 'error',
+  '@typescript-eslint/prefer-optional-chain': 'error',
+  '@typescript-eslint/no-unnecessary-condition': 'error',
+  '@typescript-eslint/consistent-type-definitions': 'error',
+  '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+  '@typescript-eslint/consistent-indexed-object-style': 'error',
+  '@typescript-eslint/consistent-generic-constructors': 'error',
+  '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+  '@typescript-eslint/prefer-nullish-coalescing': 'error',
+  '@typescript-eslint/restrict-plus-operands': [
+    'error',
+    {
+      allowNumberAndString: false,
+      skipCompoundAssignments: false,
+    },
+  ],
+  '@typescript-eslint/method-signature-style': 'error',
+  '@typescript-eslint/switch-exhaustiveness-check': 'error',
+  '@typescript-eslint/no-confusing-void-expression': 'error',
+  '@typescript-eslint/no-redundant-type-constituents': 'error',
+  '@typescript-eslint/consistent-type-imports': 'error',
+  '@typescript-eslint/consistent-type-exports': [
+    'error',
+    {
+      fixMixedExportsWithInlineTypeSpecifier: true,
+    },
+  ],
+  '@typescript-eslint/require-array-sort-compare': [
+    'error',
+    {
+      ignoreStringArrays: true,
+    },
+  ],
+  '@typescript-eslint/no-shadow': [
+    'error',
+    {
+      hoist: 'all',
+      allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+      ignoreTypeValueShadow: true,
+      ignoreFunctionTypeParameterNameValueShadow: true,
+    },
+  ],
+  '@typescript-eslint/return-await': 'error',
+  '@stylistic/padding-line-between-statements': [
+    'error',
+    {
+      blankLine: 'always',
+      prev: ['case', 'default', 'block', 'block-like', 'multiline-block-like', 'interface', 'type', 'export'],
+      next: '*',
+    },
+    {
+      blankLine: 'any',
+      prev: ['const', 'let'],
+      next: ['const', 'let'],
+    },
+    {
+      blankLine: 'always',
+      prev: '*',
+      next: ['switch', 'while', 'try', 'return', 'if', 'interface', 'type', 'function'],
+    },
+  ],
 };
