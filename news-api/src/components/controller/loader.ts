fa66e0d4a9ad94d032d6d337b95edfd5383a50c5
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-02-26 16:15:16 +0100

    feat: migrate loader.js to TS in progress

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -13,15 +13,17 @@ class Loader {
   getResp(
     { endpoint, options = {} }: { endpoint: Endpoints; options: ApiConfig },
     callback: CallbackFunction = () => {
+      // eslint-disable-next-line no-console
       console.error('No callback for GET response');
     }
   ) {
     this.load('GET', endpoint, callback, options);
   }
 
-  errorHandler(res) {
+  static errorHandler(res: Response): Response {
     if (!res.ok) {
       if (res.status === 401 || res.status === 404)
+        // eslint-disable-next-line no-console
         console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
       throw Error(res.statusText);
     }
@@ -29,12 +31,14 @@ class Loader {
     return res;
   }
 
-  makeUrl(options: ApiConfig, endpoint) {
-    const urlOptions = { ...this.options, ...options };
-    let url = `${this.baseLink}${endpoint}?`;
+  makeUrl(options: ApiConfig, endpoint: Endpoints) {
+    const urlOptions: { apiKey?: string | undefined } = { ...this.options, ...options };
+    let url: string = `${this.baseLink}${endpoint}?`;
 
     Object.keys(urlOptions).forEach((key) => {
-      url += `${key}=${urlOptions[key]}&`;
+      if (urlOptions[key]) {
+        url += `${key}=${urlOptions[key]}&`;
+      }
     });
 
     return url.slice(0, -1);
@@ -42,9 +46,10 @@ class Loader {
 
   load(method, endpoint, callback, options = {}) {
     fetch(this.makeUrl(options, endpoint), { method })
-      .then(this.errorHandler)
+      .then((res) => Loader.errorHandler(res))
       .then((res) => res.json())
       .then((data) => callback(data))
+      // eslint-disable-next-line no-console
       .catch((err) => console.error(err));
   }
 }
