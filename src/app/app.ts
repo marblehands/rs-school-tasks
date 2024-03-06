a8420455ef4659e549b2022dab22bc95fad791c1
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-03-06 20:54:45 +0100

    feat: add styles

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+export default class App {
+  private header: Header;
+
+  private main: Main;
+
+  private footer: Footer;
+
+  constructor() {
+    this.header = null;
+    this.main = null;
+    this.footer = null;
+  }
+}
