e17409f25fe41b177f66cd6e8d8e860aa0f223ba
Anna Chebysheva <anna.chebysheva@gmail.com>
2024-03-08 17:25:55 +0100

    feat: implement logout button

--- src/app/view/header/header.ts
+++ src/app/view/header/header.ts
@@ -3,22 +3,26 @@ import './header.css';
 import BaseComponent from '../../components/baseComponent/baseComponent';
 import { div, p } from '../../components/tags/tags';
 import params from './params';
+import LogoutButton from '../../components/logoutButton/logoutButton';
 
 const { HEADER, DIV, P } = params;
 
 export default class Header {
   public header: BaseComponent;
 
+  public logoutButton: LogoutButton;
+
   constructor() {
     this.header = new BaseComponent(HEADER);
+    this.logoutButton = new LogoutButton();
     this.createHeader();
   }
 
   private createHeader(): void {
-    const wrapper = div(DIV.classes).element;
+    const wrapper = div(DIV.classes);
     const paragraph = p(P.classes, P.content).element;
 
     wrapper.append(paragraph);
-    this.header.append(wrapper);
+    this.header.appendChildren([wrapper.element, this.logoutButton.element]);
   }
 }
--- src/app/view/main/params.ts
+++ src/app/view/main/params.ts
@@ -3,19 +3,6 @@ const params = {
     tag: 'main',
     classes: ['main'],
   },
-  DIV_WRAPPER: {
-    classes: ['login__wrapper'],
-  },
-  DIV_LEFT: {
-    classes: ['login__left-aside'],
-  },
-  DIV_RIGHT: {
-    classes: ['login__form-wrapper'],
-  },
-  H2: {
-    classes: ['login__form-title'],
-    content: 'Login & Welcome 🧩',
-  },
 };
 
 export default params;
