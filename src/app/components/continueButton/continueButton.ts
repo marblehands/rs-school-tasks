436f2ab3d97b14b66418ce2094cb463b619750c9
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-13 21:45:09 +0100

    feat: RSS-PZ-12 Implement Check Button (#81)
    * feat: add Check button, toggle its state and implement puzzle sequence check by click
    
    * feat: add styles for wrong and correct puzzles

--- src/app/components/continueButton/continueButton.ts
+++ src/app/components/continueButton/continueButton.ts
@@ -1,16 +1,12 @@
 import './continueButton.css';
 import BaseComponent from '../baseComponent/baseComponent';
 
-function startButtonClickHandler(): void {}
-
 export default class ContinueButton extends BaseComponent {
   constructor() {
     super({
       tag: 'button',
       classes: ['button', 'button-continue', 'disabled'],
       content: 'Continue',
-      event: 'click',
-      callback: startButtonClickHandler,
     });
   }
 }
